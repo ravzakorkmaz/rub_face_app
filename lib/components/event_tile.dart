b9593fd0ec268cae6756c28765107dd561ad57c1
Ravza Korkmaz <ravza.korkmaz@ruhr-uni-bochum.de>
2023-11-25 14:31:51 +0100

    eventtile sizes prozentual

--- lib/components/event_tile.dart
+++ lib/components/event_tile.dart
@@ -18,56 +18,58 @@ class EventTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 25),
-      padding: EdgeInsets.all(8),
-      width: 200,
-      decoration: BoxDecoration(
-        //color: Color.fromARGB(255, 61, 91, 212),
-        //color: Color.fromARGB(255, 23, 54, 92),
-        color: Color.fromARGB(255, 146, 192, 29),
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          GestureDetector(
-            child: Image.asset(imagePath),
-            onTap: details,
-          ),
-          Text(name,
-              style: const TextStyle(
-                fontSize: 18,
-                color: Colors.white,
-              )),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                price,
-                style: TextStyle(
+    return FractionallySizedBox(
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.5,
+        height: MediaQuery.of(context).size.height * 0.3,
+        margin: EdgeInsets.symmetric(horizontal: 25),
+        padding: EdgeInsets.all(8),
+        //width: 200,
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 146, 192, 29),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GestureDetector(
+              child: Image.asset(imagePath),
+              onTap: details,
+            ),
+            Text(name,
+                style: const TextStyle(
+                  fontSize: 18,
                   color: Colors.white,
+                )),
+            //SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  price,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              Row(
-                children: [
-                  Text(
-                    rating,
-                    style: TextStyle(
-                      color: Colors.white,
+                Row(
+                  children: [
+                    Text(
+                      rating,
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                  Icon(
-                    Icons.star,
-                    size: 15,
-                    color: Color.fromARGB(255, 224, 198, 6),
-                  ),
-                ],
-              )
-            ],
-          )
-        ],
+                    Icon(
+                      Icons.star,
+                      size: 15,
+                      color: Color.fromARGB(255, 224, 198, 6),
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
