35651631e516ce58809cdc7a4fc2386cb90541a8
Ravza Korkmaz <ravza.korkmaz@ruhr-uni-bochum.de>
2024-01-10 23:02:18 +0100

    endOf100124

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 import 'package:rub_face_app/components/button.dart';
 
 class StartPage extends StatelessWidget {
@@ -35,8 +35,8 @@ class StartPage extends StatelessWidget {
             Center(
               child: Image.asset(
                 "lib/images/makerspace_3d.png",
-                height: 100,
-                width: 100,
+                height: 300,
+                width: 500,
               ),
             ),
             SizedBox(height: 25),
@@ -79,6 +79,111 @@ class StartPage extends StatelessWidget {
                 myText: "Shoppe los",
                 event: () => Navigator.pushNamed(context, "/menupage"),
               ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+*/
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:rub_face_app/components/button.dart';
+
+class StartPage extends StatelessWidget {
+  const StartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 0, 53, 96),
+      body: SafeArea(
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.asset(
+              "lib/images/makerspace_3d.png",
+              fit: BoxFit.cover,
+            ),
+            Container(
+              color:
+                  Colors.black.withOpacity(0.5), // Adjust the opacity as needed
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                    sigmaX: 5, sigmaY: 5), // Adjust the blur intensity
+                child: Container(
+                  color: Colors.transparent,
+                ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 15),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "MAKERSPACE",
+                      style: TextStyle(
+                        fontSize: 38,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    /*Image.asset(
+                      "lib/images/japanflag.png",
+                      height: 30,
+                    ),*/
+                  ],
+                ),
+                SizedBox(height: 50),
+                SizedBox(height: 25),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Text(
+                    "Erleben Sie kreative Produkte!",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Text(
+                    "クリエイティブな製品を体験してください",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+                SizedBox(height: 35),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Text(
+                    "Entdecke die Welt der Entfaltung und tauche ein in das Meer voller Ideen.",
+                    style: TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: MyButton(
+                    myText: "Shoppe los",
+                    event: () => Navigator.pushNamed(context, "/menupage"),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
