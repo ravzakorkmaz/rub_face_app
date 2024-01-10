32a576054f61de3ee00c229bc070b24d53bb1e42
Ravza Korkmaz <ravza.korkmaz@ruhr-uni-bochum.de>
2024-01-10 20:37:34 +0100

    testtest

--- lib/pages/event_pages/product.dart
+++ lib/pages/event_pages/product.dart
@@ -1,176 +1,3 @@
-/*import 'package:flutter/material.dart';
-import 'package:rub_face_app/cart_page.dart';
-import 'package:rub_face_app/components/app_bar.dart';
-import 'package:rub_face_app/components/button.dart';
-import 'package:provider/provider.dart';
-import 'package:rub_face_app/models/cart_model.dart';
-
-class ProductPage extends StatelessWidget {
-  final Map<String, dynamic> product;
-  final List<Map<String, dynamic>> productList;
-  final CartModel cart = CartModel();
-
-  ProductPage({required this.product, required this.productList});
-
-  @override
-  Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
-    return FractionallySizedBox(
-      child: Consumer<CartModel>(
-        builder: (context, cartModel, child) => Scaffold(
-          appBar: MyAppBar(
-            myTitle: 'M A K E R S P A C E',
-            productList: productList,
-          ),
-          backgroundColor: Colors.white,
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Image.asset(
-                  product['imagePath'],
-                  height: 250,
-                ),
-              ),
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.star,
-                      color: Colors.amber,
-                      size: 30,
-                    ),
-                    Text(
-                      product['rating'],
-                      style: TextStyle(
-                        fontSize: 20,
-                        //color: Colors.white,
-                        //color: Color.fromARGB(255, 23, 54, 92),
-                        color: darkModeProvider.isDarkMode ? Colors.white : Color.fromARGB(255, 23, 54, 92),
-
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 8),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Text(
-                  product['productName'],
-                  style: TextStyle(
-                    fontSize: 28,
-                    //color: Colors.white,
-                    color: Color.fromARGB(255, 42, 53, 66),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Text(
-                  "Test Text wo taucht das auf?",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    //color: Colors.white,
-                    color: Color.fromARGB(255, 23, 54, 92),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 8),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Text(
-                  product['description'],
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Color.fromARGB(255, 23, 54, 92),
-                    height: 1,
-                  ),
-                ),
-              ),
-              SizedBox(height: screenHeight * 0.05),
-              Expanded(
-                child: Container(
-                  height: MediaQuery.of(context).size.height * 0.4,
-                  padding: EdgeInsets.all(25),
-                  color: Color.fromARGB(255, 23, 54, 92),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(
-                            product['price'],
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Row(
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.white,
-                                ),
-                                child: IconButton(
-                                    onPressed: () =>
-                                        cartModel.removeFromCart(product),
-                                    icon: Icon(Icons.remove)),
-                              ),
-                              const SizedBox(width: 8),
-                              Text(
-                                cartModel
-                                    .getProductQuantity(product)
-                                    .toString(),
-                                style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              const SizedBox(width: 8),
-                              Container(
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.white,
-                                ),
-                                child: IconButton(
-                                    onPressed: () =>
-                                        cartModel.addToCart(product),
-                                    icon: Icon(Icons.add)),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                      const SizedBox(height: 20),
-                      MyButton(
-                        myText:
-                            "Zum Einkaufswagen (${cartModel.getTotalItems()})",
-                        event: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  CartPage(productList: productList),
-                            )),
-                      )
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-*/
-
 import 'package:flutter/material.dart';
 import 'package:rub_face_app/cart_page.dart';
 import 'package:rub_face_app/components/app_bar.dart';
@@ -198,8 +25,8 @@ class ProductPage extends StatelessWidget {
             productList: productList,
           ),
           backgroundColor: darkModeProvider.isDarkMode
-              ? Color.fromARGB(255, 4, 17, 36)
-              : Colors.white,
+              ? Color.fromARGB(255, 14, 20, 31)
+              : Color.fromARGB(255, 231, 231, 231),
           body: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -225,7 +52,7 @@ class ProductPage extends StatelessWidget {
                         fontSize: 20,
                         color: darkModeProvider.isDarkMode
                             ? Colors.white
-                            : Color.fromARGB(255, 23, 54, 92),
+                            : Color.fromARGB(255, 0, 53, 96),
                       ),
                     ),
                   ],
@@ -254,7 +81,7 @@ class ProductPage extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     color: darkModeProvider.isDarkMode
                         ? Colors.white
-                        : Color.fromARGB(255, 23, 54, 92),
+                        : Color.fromARGB(255, 0, 53, 96),
                   ),
                 ),
               ),
@@ -267,7 +94,7 @@ class ProductPage extends StatelessWidget {
                     fontSize: 16,
                     color: darkModeProvider.isDarkMode
                         ? Colors.white
-                        : Color.fromARGB(255, 23, 54, 92),
+                        : Color.fromARGB(255, 0, 53, 96),
                     height: 1,
                   ),
                 ),
@@ -279,8 +106,8 @@ class ProductPage extends StatelessWidget {
                   padding: EdgeInsets.all(25),
                   /*color: darkModeProvider.isDarkMode
                       ? Colors.black
-                      : Color.fromARGB(255, 23, 54, 92),*/
-                  color: Color.fromARGB(255, 23, 54, 92),
+                      : Color.fromARGB(255,0,53,96),*/
+                  color: Color.fromARGB(255, 0, 53, 96),
                   child: Column(
                     children: [
                       Row(
